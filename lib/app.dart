6447557235768fb5f2feb087c6dc3afb9fca2cab
enes <abdullahogluenes@gmail.com>
2023-02-07 02:43:49 +0300

    skeleton

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DepremDestekApp extends StatefulWidget {
+  const DepremDestekApp({super.key});
+
+  @override
+  State<DepremDestekApp> createState() => _DepremDestekAppState();
+}
+
+class _DepremDestekAppState extends State<DepremDestekApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
