95969baad2889a16b8e258e69baccd5c8e488ab4
Nihatcan DALYANLAR <ndalyanlar@gmail.com>
2023-02-08 23:20:36 +0300

    error page created

--- lib/pages/app_load_failure_page/app_load_failure_page.dart
+++ lib/pages/app_load_failure_page/app_load_failure_page.dart
@@ -1,5 +1,3 @@
-import 'dart:ui' if (dart.library.html) 'dart:html' as html;
-
 import 'package:deprem_destek/gen/assets.gen.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
