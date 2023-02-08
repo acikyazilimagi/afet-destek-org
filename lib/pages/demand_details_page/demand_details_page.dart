8d81cc869f2beab8b4ff4c7dbca83d383002a43d
Davut Engin <davutengin@hotmail.com>
2023-02-09 00:35:01 +0300

    demand detail fix

--- lib/pages/demand_details_page/demand_details_page.dart
+++ lib/pages/demand_details_page/demand_details_page.dart
@@ -1,6 +1,6 @@
 import 'package:deprem_destek/data/models/demand.dart';
 import 'package:deprem_destek/data/repository/auth_repository.dart';
-import 'package:deprem_destek/pages/demand_details_page/widgets/sms_button.dart';
+import 'package:deprem_destek/gen/assets.gen.dart';
 import 'package:deprem_destek/pages/demand_details_page/widgets/whatsapp_button.dart';
 import 'package:deprem_destek/pages/demands_page/widgets/demand_card.dart';
 import 'package:deprem_destek/shared/state/app_cubit.dart';
@@ -9,6 +9,9 @@ import 'package:deprem_destek/shared/widgets/loader.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
+
+import 'widgets/call_button.dart';
 
 class DemandDetailsPage extends StatelessWidget {
   const DemandDetailsPage._({required this.demand});
@@ -64,39 +67,53 @@ class _DemandDetailsPageView extends StatelessWidget {
     const dummyUserIdentified = true;
 
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const SizedBox(height: 30),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Text(
-              'Talep Detayı',
-              maxLines: 1,
-              style: TextStyle(
-                fontWeight: FontWeight.w600,
-                fontSize: 24,
-                color: Color(0xff101828),
+      appBar: AppBar( actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 16),
+          child: SvgPicture.asset(Assets.logoSvg),
+        )
+      ],),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 30),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: Text(
+                'Talep Detayı',
+                maxLines: 1,
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                  color: Color(0xff101828),
+                ),
               ),
             ),
-          ),
-          DemandCard(demand: demand, isDetail: false),
-          // TODO(resultanyildizi): if the user is not identified
-          // TODO(resultanyildizi): show the warning box
-          if (dummyUserIdentified) ...[
-            const Infobox(
-              info: '''
-                  Bu kişinin kimliği tarafımızca doğrulanmamıştır.
-                  Lütfen dikkatli olunuz.''',
+            DemandCard(demand: demand, isDetail: true),
+            // TODO(resultanyildizi): if the user is not identified
+            // TODO(resultanyildizi): show the warning box
+            if (dummyUserIdentified) ...[
+              const Infobox(
+                info: '''
+                    Bu kişinin kimliği tarafımızca doğrulanmamıştır.
+                    Lütfen dikkatli olunuz.''',
+              ),
+              const SizedBox(height: 15),
+            ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: WhatsappButton(phoneNumber: demand.phoneNumber),
             ),
-            const SizedBox(height: 15),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: CallButton(phoneNumber: demand.phoneNumber),
+            ),
+            const SizedBox(height: 30),
+            const Center(child: Text('GSM operatörlerindeki yoğunluk sebebiyle SMS kullanmayı tercih ediniz.'))
           ],
-          WhatsappButton(phoneNumber: demand.phoneNumber),
-          const SizedBox(height: 15),
-          SmsButton(phoneNumber: demand.phoneNumber),
-          const SizedBox(height: 30),
-          const Center(child: Text('Lütfen aramayı tercih etmeyiniz'))
-        ],
+        ),
       ),
     );
   }
