daa8446e2dabab00bf7db2ef477853db153263e9
bmercan <bayram.178@gmail.com>
2023-02-08 17:31:55 +0300

    flutter analyze resolve

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:deprem_destek/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:url_strategy/url_strategy.dart'; 
 import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
