cf67fbf181bd26b80118a1a9922a80cc53f3c560
Furkan Acar <facarr00@gmail.com>
2023-02-08 20:04:07 +0300

    introduction page added

--- lib/pages/introduction_page/introduction_page.dart
+++ lib/pages/introduction_page/introduction_page.dart
@@ -9,40 +9,90 @@ class IntroductionPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset(
-                Assets.logoSvg,
-                height: 75,
-                width: 75,
-              ),
-              const SizedBox(height: 16),
-              Text(
-                'Bu platform lokasyon verinizi kullanarak çalışmaktadır. \n'
-                'Lütfen cihazınızdaki konum servisini \n'
-                'aktif ettiğinizden emin olunuz.',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                      fontSize: 20,
-                    ),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  fixedSize: const Size(double.maxFinite - 40, 50),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          //  mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // mainAxisSize: MainAxisSize.max,
+          children: [
+            SizedBox(height: height * 0.2),
+            buildLogo(height),
+            SizedBox(height: height * 0.05),
+            buildTitle(context),
+            SizedBox(height: height * 0.05),
+            buildContent(context),
+            SizedBox(height: height * 0.05),
+            SizedBox(
+              width: width * .8,
+              child: buildRequestButton(context, width, height),
+            ),
+            SizedBox(height: height * 0.05),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: buildKvkkTextButton(context),
                 ),
-                onPressed: () => context.read<AppCubit>().startApp(),
-                child: const Text('Konum izni ver'),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  TextButton buildKvkkTextButton(BuildContext context) {
+    return TextButton(
+        onPressed: () {},
+        style: TextButton.styleFrom(
+            foregroundColor: Theme.of(context).textTheme.titleSmall?.color),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text("KVKK Açık Rıza Metni'ni oku "),
+            Icon(Icons.arrow_forward),
+          ],
+        ));
+  }
+
+  ElevatedButton buildRequestButton(
+      BuildContext context, double width, double height) {
+    return ElevatedButton(
+      onPressed: () => context.read<AppCubit>().startApp(),
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(width * .8, 50),
+        maximumSize: Size(width * .8, 70),
+      ),
+      child: const Text('Konum izni ver'),
+    );
+  }
+
+  Text buildContent(BuildContext context) {
+    return Text(
+      'Bu platform lokasyon verinizi kullanarak çalışmaktadır.\n'
+      'Lütfen cihazınızdaki konum servisini \n'
+      'aktif ettiğinizden emin olunuz.',
+      textAlign: TextAlign.center,
+      style: Theme.of(context).textTheme.titleSmall,
+    );
+  }
+
+  Text buildTitle(BuildContext context) {
+    return Text('Afet Destek Platformu',
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.headlineSmall);
+  }
+
+  SvgPicture buildLogo(double height) {
+    return SvgPicture.asset(
+      Assets.logoSvg,
+      height: height * .15,
     );
   }
 }
