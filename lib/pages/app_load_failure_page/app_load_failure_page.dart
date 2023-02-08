6d2e2df4ec540ded78c3ace72ce33acef90efe27
Nihatcan DALYANLAR <ndalyanlar@gmail.com>
2023-02-08 23:17:47 +0300

    error page created

--- lib/pages/app_load_failure_page/app_load_failure_page.dart
+++ lib/pages/app_load_failure_page/app_load_failure_page.dart
@@ -1,10 +1,16 @@
+import 'dart:ui' if (dart.library.html) 'dart:html' as html;
+
+import 'package:deprem_destek/gen/assets.gen.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AppLoadFailurePage extends StatelessWidget {
   const AppLoadFailurePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     // implement try again button that calls AppCubit.load again
     // implement a location required warning in the page
     return Scaffold(
@@ -12,15 +18,84 @@ class AppLoadFailurePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Bu sayfa yapım aşamasındadır.'),
-            const Text('Lokasyon izni verdiğinizden emin olunuz.'),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // reload web window when this is pressed
-              },
-              child: const Text('Tekrar Dene'),
-            )
+            SizedBox.square(
+              dimension: size.height * .2,
+              child: SvgPicture.asset(Assets.logoSvg),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              child: Text(
+                'Afet Destek Platformu',
+                style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 22,
+                    ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                'Konum izni verdiğinizden emin olun.',
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              width: size.width,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (kIsWeb) {
+                      //TODO refresh
+                    } else {
+                      //TODO for mobile action
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 6,
+                    ),
+                    child: Text(
+                      'Sayfayı Yenile',
+                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: size.width,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: const BorderSide(
+                      width: 2,
+                      color: Colors.red,
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 6,
+                    ),
+                    child: Text(
+                      'Konumumu nasıl açarım?',
+                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                            fontSize: 16,
+                            color: Colors.red,
+                          ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
