7a79893e6ba82b1a217558b060c5c2446a3c508f
Nihatcan DALYANLAR <ndalyanlar@gmail.com>
2023-02-09 19:45:45 +0300

    phone number length checked for Add/Edit Demand

--- lib/shared/widgets/reactive_intl_phone_field.dart
+++ lib/shared/widgets/reactive_intl_phone_field.dart
@@ -3,14 +3,19 @@ import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 
 class ReactiveIntlPhoneField extends StatelessWidget {
-  const ReactiveIntlPhoneField({required this.formControl, super.key});
+  const ReactiveIntlPhoneField({
+    required this.formControl,
+    this.invalidNumberMessage,
+    super.key,
+  });
   final FormControl<String> formControl;
-
+  final String? invalidNumberMessage;
   @override
   Widget build(BuildContext context) {
     return ReactiveFormConsumer(
       builder: (context, form, _) {
         return IntlPhoneField(
+          invalidNumberMessage: invalidNumberMessage,
           initialValue: formControl.value,
           onChanged: (value) => formControl.value = value.completeNumber,
         );
