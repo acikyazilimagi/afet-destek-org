9495d230d952bc37264e6fcc2f488a3d8927604e
bugragoksu <bugragoksu2@gmail.com>
2023-02-10 22:58:22 +0300

    extract appbar

--- lib/pages/demands_page/demands_page.dart
+++ lib/pages/demands_page/demands_page.dart
@@ -1,20 +1,17 @@
 import 'package:afet_destek/data/repository/auth_repository.dart';
 import 'package:afet_destek/data/repository/demands_repository.dart';
-import 'package:afet_destek/gen/assets.gen.dart';
-import 'package:afet_destek/pages/auth_page/auth_page.dart';
 import 'package:afet_destek/pages/demands_page/state/demands_cubit.dart';
 import 'package:afet_destek/pages/demands_page/widgets/demand_filter_popup.dart';
+import 'package:afet_destek/pages/demands_page/widgets/demands_page_appbar.dart';
 import 'package:afet_destek/pages/demands_page/widgets/generic_grid_list.dart';
 import 'package:afet_destek/pages/demands_page/widgets/list_view_responsive.dart';
 import 'package:afet_destek/pages/demands_page/widgets/mobile_list_view.dart';
-import 'package:afet_destek/pages/my_demand_page/my_demand_page.dart';
 import 'package:afet_destek/shared/state/app_cubit.dart';
 import 'package:afet_destek/shared/theme/color_extensions.dart';
 import 'package:afet_destek/shared/widgets/loader.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class DemandsPage extends StatelessWidget {
   const DemandsPage({super.key});
@@ -79,74 +76,9 @@ class _DemandsPageViewState extends State<_DemandsPageView> {
     // TODO(enes): failure page for failure state
 
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10),
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-              ),
-              onPressed: !widget.isAuthorized
-                  ? () {
-                      AuthPage.show(
-                        context,
-                        onClose: () {
-                          context.read<DemandsCubit>().refreshDemands();
-                        },
-                      );
-                    }
-                  : () {
-                      MyDemandPage.show(
-                        context,
-                        onClose: () {
-                          context.read<DemandsCubit>().refreshDemands();
-                        },
-                      );
-                    },
-              child: Text(
-                widget.isAuthorized ? 'Destek Talebim' : 'Talep Oluştur',
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: context.appColors.white,
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(width: 12),
-          Builder(
-            builder: (ctx) {
-              return IconButton(
-                icon: Stack(
-                  children: [
-                    const Icon(Icons.filter_list),
-                    if (state.hasAnyFilters) ...[
-                      Positioned(
-                        top: 0,
-                        right: 0,
-                        child: Container(
-                          width: 8,
-                          height: 8,
-                          decoration: BoxDecoration(
-                            color: context.appColors.mainRed,
-                            shape: BoxShape.circle,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ],
-                ),
-                onPressed: () => Scaffold.of(ctx).openEndDrawer(),
-              );
-            },
-          ),
-          const SizedBox(width: 8),
-        ],
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 16),
-          child: SvgPicture.asset(Assets.logoSvg),
-        ),
+      appBar: DemandPageAppBar(
+        isAuthorized: widget.isAuthorized,
+        hasAnyFilters: state.hasAnyFilters,
       ),
       body: demands.isEmpty
           ? Center(
