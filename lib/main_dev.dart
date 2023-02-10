36ae1cc97a7afc8b2e4e2278c040dc1f2499da06
adnan <adnanfahedhr@gmail.com>
2023-02-10 21:14:31 +0300

    fix: ios shows blank page
    this was being causes because of a firebase error, this commit implements a temporary error, and also adds a TODO for a better fix if there is any

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,23 +1,8 @@
 import 'package:afet_destek/app_func.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  //catch Unhandled exceptions and errors
-  await AppFunc.start(
-    url: 'https://us-central1-env-deprem-destek-org.cloudfunctions.net/',
-    options: const FirebaseOptions(
-      apiKey: 'AIzaSyAwga9Tzl9ODSGdVRNtw8lgAS5MoIl16Ck',
-      appId: '1:643767075935:web:3cc9d6c4658af4ded7c7ea',
-      messagingSenderId: '643767075935',
-      projectId: 'env-deprem-destek-org',
-      authDomain: 'env-deprem-destek-org.firebaseapp.com',
-      storageBucket: 'env-deprem-destek-org.appspot.com',
-      measurementId: 'G-G552720SJK',
-    ),
-    sentryDsn:
-        'https://a472fdea96db4bd3b3ca80ce8583e9ba@o4504644634607616.ingest.sentry.io/4504651796381696',
-  );
+  await AppFunc.startDev();
 }
