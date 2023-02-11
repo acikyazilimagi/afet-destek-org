02d746dbc3b66de282fa96de07bacca815d30ec9
enes <abdullahogluenes@gmail.com>
2023-02-11 17:15:29 +0300

    change demand category model

--- lib/data/models/demand_category.dart
+++ lib/data/models/demand_category.dart
@@ -8,8 +8,16 @@ class DemandCategory with _$DemandCategory {
   const factory DemandCategory({
     required String id,
     required String name,
+    @JsonKey(name: 'name_ar') required String nameAr,
+    @JsonKey(name: 'name_en') required String nameEn,
   }) = _DemandCategory;
+
+  const DemandCategory._();
 
   factory DemandCategory.fromJson(Map<String, dynamic> json) =>
       _$DemandCategoryFromJson(json);
+
+  String localizedName(/*lang arg here*/) {
+    return '';
+  }
 }
