5c9d9abe0b6b26fbeea19a023dd64dcc37fafa55
enes <abdullahogluenes@gmail.com>
2023-02-11 03:02:52 +0300

    version 1.0.4

--- web/loader/loader.js
+++ web/loader/loader.js
@@ -233,7 +233,7 @@ _flutter.loader = null;
      */
     async loadEntrypoint(options) {
       const {
-        entrypointUrl = "main.dart.js?version=1.0.3",
+        entrypointUrl = "main.dart.js?version=1.0.4",
         onEntrypointLoaded,
       } = options || {};
 
