8a66a7474655341629f8a5a6c38a37749c22354e
enes <abdullahogluenes@gmail.com>
2023-02-15 12:40:32 +0000

    revert

--- lib/data/models/demand_category.dart
+++ lib/data/models/demand_category.dart
@@ -3,22 +3,13 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'demand_category.freezed.dart';
 part 'demand_category.g.dart';
 
-
 @freezed
 class DemandCategory with _$DemandCategory {
   const factory DemandCategory({
     required String id,
     required String name,
-    @JsonKey(name: 'name_ar') required String nameAr,
-    @JsonKey(name: 'name_en') required String nameEn,
   }) = _DemandCategory;
-
-  const DemandCategory._();
 
   factory DemandCategory.fromJson(Map<String, dynamic> json) =>
       _$DemandCategoryFromJson(json);
-
-  String localizedName(/*lang arg here*/) {
-    return '';
-  }
 }
